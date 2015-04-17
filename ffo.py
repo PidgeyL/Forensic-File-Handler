5e811e3389c8928ac92be33fd4f462ad12956a12
PidgeyL <pieterjan.moreels@gmail.com>
2015-04-17 10:43:58 +0200

    adding magics

--- ffo.py
+++ ffo.py
@@ -37,13 +37,31 @@ Magic                         - ASCII      - Ident - Extension          |  Magic
 50 4B 03 04 14 00 06 00       - PK......   - OLE   - .docx, pptx, xlsx  |  D0 CF 11 E0       - ....   - OLE   - .doc, .xls, .ppt
 50 4B 03 04 14 00 08 00 08 00 - PK........ - JAR   - .jar               |  49 44 33          - ID3    - MP3   - .mp3
 50 4B 03 04                   - PK..       - ZIP   - .zip               |  4D 5A             - MZ     - EXE   - .exe
-89 50 4E 47 0D 0A 1A 0A       - .PNG....   - PNG   - .png
+89 50 4E 47 0D 0A 1A 0A       - .PNG....   - PNG   - .png               |  1A 45 DF A3       - .E..   - WEBM  - .webm
+37 7A BC AF 27 1C             - 7z..'.     - 7ZIP  - .7z                |  42 4D             - BM     - BMP   - .bmp
+43 44 30 30 31                - CD001      - ISO   - .iso               |  43 57 53          - CWS    - SWF   - .swf
+46 4C 56 01                   - FLV.       - FLV   - .flv               |  46 57 53          - FWS    - SWF   - .swf
+5F 27 A8 89                   - _'..       - JAR   - .jar               |  5A 57 53          - ZWS    - SWF   - .swf
+66 4C 61 43 00 00 00 22       - fLaC..."   - FLAC  - flac               |  7E 74 2C 01       - ~t,.   - IMG   - .img
+FF                            - .          - SYS   - .sys               |  ED AB EE DB       - ....   - RPM   - .rpm
+FF 4B 45 59 42 20 20 20       - .KEYB      - SYS   - .sys               |  CA FE BA BE       - ....   - CLASS - .class
+FF FF FF FF                   - ....       - SYS   - .sys
+
+Magic                                                                   - ASCII                    - Ident - Extension
+00 00 00 14 66 74 79 70 71 74 20 20                                     - ....ftypqt               - MOV   - .mov
+00 00 00 14 66 74 79 70 69 73 6F 6D                                     - ....ftypisom             - MP4   - .mp4
+00 00 00 18 66 74 79 70 33 67 70 35                                     - ....ftyp3gp5             - MP4   - .mp4
+00 00 00 1C 66 74 79 70 4D 53 4E 56 01 29 00 46 4D 53 4E 56 6D 70 34 32 - ....ftypMSNV.).FMSNVmp42 - MP4   - .mp4
 '''
 
 magics={"52 61 72 21 1A 07 00":"RAR",          "52 61 72 21 1A 07 01 00":"RAR", "50 4B 03 04 0A 00 02 00":"EPUB", "50 4B 03 04 14 00 06 00":"OLE",
         "50 4B 03 04 14 00 08 00 08 00":"JAR", "50 4B 03 04":"ZIP",             "89 50 4E 47 0D 0A 1A 0A":"PNG",  "47 49 46 38 37 61":"GIF",
-        "47 49 46 38 39 61":"GIF",
-        "25 50 44 46":"PDF", "D0 CF 11 E0":"OLE", "49 44 33":"MP3", "4D 5A":"EXE"}
+        "47 49 46 38 39 61":"GIF",  "25 50 44 46":"PDF", "D0 CF 11 E0":"OLE",    "49 44 33":"MP3", "4D 5A":"EXE",    "1A 45 DF A3":"WEBM",
+        "37 7A BC AF 27 1C":"7ZIP", "46 4C 56 01":"FLV", "43 44 30 30 31":"ISO", "43 57 53":"SWF", "46 57 53":"SWF", "5A 57 53":"SWF",
+        "42 4D":"BMP",              "5F 27 A8 89":"JAR", "7E 74 2C 01":"IMG",    "66 4C 61 43 00 00 00 22":"FLAC",   "CA FE BA BE":"CLASS",
+        "ED AB EE DB":"RPM",        "FF":"SYS",          "FF FF FF FF":"SYS",    "FF 4B 45 59 42 20 20 20":"SYS", 
+        "00 00 00 14 66 74 79 70 71 74 20 20":"MOV",    "00 00 00 14 66 74 79 70 69 73 6F 6D":"MP4",    "00 00 00 18 66 74 79 70 33 67 70 35":"MP4",
+        "00 00 00 1C 66 74 79 70 4D 53 4E 56 01 29 00 46 4D 53 4E 56 6D 70 34 32":"MP4"}
 
 # python2 and 3 compatible functions
 def toString(b):
